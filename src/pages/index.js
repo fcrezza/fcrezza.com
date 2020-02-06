34536b142f7be552d1e0334690cfa0ee0b99c0c1
Anang Fachreza <fachrezanang@gmail.com>
2020-02-06 14:58:22 +0700

    fix oversize icon on firefox

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {config} from '@fortawesome/fontawesome-svg-core'
 import SEO from '../components/seo'
 import Nav from '../components/Nav'
 import Layout from '../components/Layout'
@@ -9,6 +10,10 @@ import Portfolio from '../components/Portfolio'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 import {ThemeProvider} from '../utils/ThemeContext'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+// Prevent fontawesome from dynamically adding its css
+config.autoAddCss = false
 
 const IndexPage = () => (
   <>
